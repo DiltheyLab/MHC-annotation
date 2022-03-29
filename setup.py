b1e8a3f24830e84dd215aa25a327631fe8b3650b
torhou <toho@gmx.de>
2022-03-29 17:44:00 +0200

    Pypi version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MHC-Annotation',
-    version='0.0.6',
+    version='0.0.8',
     license='MIT',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
